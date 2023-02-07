5135e11869e4a0e0bd090dac62b55cff509e59b1
ShariqAli-Dev <shariqali.dev@gmail.com>
2023-02-06 21:25:21 -0500

    Reformatted application of express server  - changed file names and organized into folders  - added comments for easier understanding

---
+++ api/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+
+import assetRouter from "./routers/asset/asset-router.js";
+import marketRouter from "./routers/market/market-router.js";
+
+const server = express();
+const port = 3000;
+
+// Middleware
+server.use(express.json());
+
+// Routers
+server.use("/asset", assetRouter);
+server.use("/market", marketRouter);
+
+// Sanity Check
+server.get("/", (req, res) => {
+  res.status(201).json({ message: "initial request succesful" });
+});
+
+// Global Error Handler
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
