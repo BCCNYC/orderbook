1f0c09bd0b3a49e68fd19ee5e9b22b9345093377
ShariqAli-Dev <shariqali.dev@gmail.com>
2023-02-08 14:43:28 -0500

    simplified redis seeding

--- api/redis.ts
+++ api/redis.ts
@@ -3,15 +3,15 @@ import { assets, markets } from "./dummyData.js";
 const redis = new Redis(process.env.REDIS_URL);
 
 // seeding redis
-if (
-  !!(await redis.smembers("markests")).length &&
-  !!(await redis.smembers("assets")).length
-) {
-  assets.forEach(async (asset) => {
-    await redis.sadd("assets", JSON.stringify(asset));
-  });
+if ((await redis.smembers("markets")).length === 0) {
   markets.forEach(async (market) => {
     await redis.sadd("markets", JSON.stringify(market));
+  });
+}
+
+if ((await redis.smembers("assets")).length === 0) {
+  assets.forEach(async (asset) => {
+    await redis.sadd("assets", JSON.stringify(asset));
   });
 }
 
