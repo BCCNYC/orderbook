6e160562191ac6b53e5dde44e23bd7c5c4299d7c
ShariqAli-Dev <shariqali.dev@gmail.com>
2023-02-08 12:56:55 -0500

    bugfix: redis continued seeding if data already existed  - added a conditonal that checks if the markets and    asses table had length. If length was 0, then    seeding did not happen. I length is > 0, seeding    already happened

--- api/redis.ts
+++ api/redis.ts
@@ -3,14 +3,16 @@ import { assets, markets } from "./dummyData.js";
 const redis = new Redis(process.env.REDIS_URL);
 
 // seeding redis
-assets.forEach(async (asset) => {
-  await redis.sadd("assets", JSON.stringify(asset));
-});
-
-markets.forEach(async (market) => {
-  await redis.sadd("markets", JSON.stringify(market));
-});
-
-
+if (
+  !!(await redis.smembers("markests")).length &&
+  !!(await redis.smembers("assets")).length
+) {
+  assets.forEach(async (asset) => {
+    await redis.sadd("assets", JSON.stringify(asset));
+  });
+  markets.forEach(async (market) => {
+    await redis.sadd("markets", JSON.stringify(market));
+  });
+}
 
 export default redis;
