546822b28b2ab4f204c03b93c69a20af57121d6e
Kaleb <kalebross30@gmail.com>
2023-02-08 14:55:22 -0500

    no change

--- api/routers/markets/markets-router.ts
+++ api/routers/markets/markets-router.ts
@@ -3,8 +3,11 @@ import redis from "../../redis.js";
 import {Market} from '../../interfaces.js'
 const router = express.Router();
 
-// fetches
+// add middleWare to check if an order crosses the book. (Matching Engine)
+// - make sure that the current buy is being made that is greater than the lowest sell, then it's a fair trade. It never enters the book because crosses the book
+// - works the other way around with sells
 
+// fetches all markets
 router.get("/", (req, rest, next) => {
   redis.smembers("markets", (error, result) => {
     if (error) {
@@ -15,9 +18,8 @@ router.get("/", (req, rest, next) => {
   });
 });
 
-// fetches orders
-
-router.get("/orders", (req, res, next) => {
+// fetches all orders from all markets
+router.get("/orders", async (req, res, next) => {
   redis.smembers("markets", (error, result) => {
     if (error) {
       next({ status: 404, message: "could not get order" });
@@ -30,5 +32,12 @@ router.get("/orders", (req, res, next) => {
     }
   });
 });
+
+// fetch all orders from x market
+router.get("/:pair/orders", async)
+
+
+// fetch the top of the book
+
 
 export default router;
