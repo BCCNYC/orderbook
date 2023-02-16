0df4119c6197ba55efd789e94821e7840edc1140
Omar Hegazy <seandarker@meta.com>
2023-02-16 12:52:24 -0500

    check for correct asset when adding order to the top of the order book

--- main.py
+++ main.py
@@ -106,12 +106,18 @@ class Top:
         self.__lowestAsks = []
         self.__highestBids = []
 
+    def get_market(self): return self.__market
+    def get_depth(self): return self.__depth
+    def get_asks(self): return self.__lowestAsks
+    def get_bids(self): return self.__highestBids
+
     def addAsk(self, price, volume):
         if len(self.__lowestAsks) < depth:
             self.__lowestAsks.append((price, volume))
             self.__lowestAsks = sorted(self.__lowestAsks, key=lambda x: x[0]) #sort by lowest ask
         if self.__lowestAsks[depth - 1][0] > price:
             self.__lowestAsks[depth - 1] = (price, volume) #replace highest ask with this ask if this ask's price is lower
+        #otherwise, this order doesn't show up in the current top of the orderbook depth
 
     def addBid(self, price, volume):
         if len(self.__highestBids) < depth:
@@ -119,12 +125,18 @@ class Top:
             self.__highestBids = sorted(self.__highestBids, key = lambda x: x[0], reverse=True)  #sort by highest bids
         if self.__highestBids[depth - 1][0] < price:
             self.__highestBids[depth - 1] = (price, volume) #replace lowest bid with this bid if this bid is higher
+        #otherwise, this order doesn't show up in the current top of the orderbook depth
 
     def addOrder(self, order: Order):
+        if order.get_market() != self.get_market():
+            order_symbol = order.get_market().get_baseAsset().get_symbol()
+            book_symbol = self.get_market().get_baseAsset().get_symbol()
+            raise ValueError(f"Trying to add order for asset {order_symbol} to order book for asset {book_symbol}")
         if order.get_side() == 1:
             self.addAsk(order.get_price(), order.get_volume()) #sell side order
         else:
             self.addBid(order.get_price(), order.get_volume()) #buy side order
+
 
 def main():
     print("OrderBook")
